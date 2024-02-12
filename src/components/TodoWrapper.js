2412e1386d6b4f83a267a9a3466ba4f6e3845e35
Manish Kumar <manish.mycodes@gmail.com>
2024-02-12 11:55:58 +0530

    Todo: Delete functionality done

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,25 +1,31 @@
-import react, {useState} from "react";
-import { TodoForm } from "./TodoForm";
-import { v4 as uuidv4 } from 'uuid';
-import { Todo } from "./Todo";
-uuidv4();
+import react, { useState } from 'react'
+import { TodoForm } from './TodoForm'
+import { v4 as uuidv4 } from 'uuid'
+import { Todo } from './Todo'
+uuidv4()
 
 export const TodoWrapper = () => {
     const [todos, setTodos] = useState([])
-    
-    const addTodo = todo => {
-        setTodos([...todos, {id: uuidv4(), task: todo, 
-            completed: false, isEditing: false}])
-            console.log(todos)
+
+    const addTodo = (todo) => {
+        setTodos([
+            ...todos,
+            { id: uuidv4(), task: todo, completed: false, isEditing: false },
+        ])
+        console.log(todos)
     }
 
-    return(
+    const deleteTodo = (id) => {
+        setTodos(todos.filter((item) => item.id !== id))
+    }
+
+    return (
         <div className="TodoWraper">
             <h1>Get things Done</h1>
             <TodoForm addTodo={addTodo} />
             {todos.map((todo, index) => (
-             <Todo task={todo} key={index} />
-            ))}    
+                <Todo task={todo} key={index} deleteTodo={deleteTodo} />
+            ))}
         </div>
     )
 }
